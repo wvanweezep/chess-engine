67087d36faf6778b1d3a29c7ea25da781b723500
Wessel van Weezep <wvanweezep@gmail.com>
2026-05-14 10:29:40 +0200

    Implemented Connect4Game interface and winning move check

---
+++ src/connect-4/arr-connect-4-game.ts
@@ -0,0 +1,148 @@
+import {Connect4Game} from "./connect-4-game";
+
+/**
+ * ## Array Connect4 Game
+ * Represents an instance of the Connect4 game implemented using a flat array.
+ */
+export class ArrayConnect4Game implements Connect4Game {
+
+    // Width of every Connect4 board.
+    public static readonly BOARD_WIDTH: number = 7;
+    // Height of every Connect4 board.
+    public static readonly BOARD_HEIGHT: number = 6;
+    // Number of slots of every Connect4 board.
+    public static readonly BOARD_SIZE: number = ArrayConnect4Game.BOARD_WIDTH * ArrayConnect4Game.BOARD_HEIGHT;
+    // Board directions to check for connect 4;
+    private static readonly DIRECTIONS: ReadonlyArray<readonly [number, number]> = [[1, 0], [0, 1], [1, 1], [1, -1]];
+
+    // Array representing the current Connect4 board state.
+    private readonly board: Uint8Array;
+    // Array indicating the current height of all columns.
+    private readonly columnHeights: number[];
+
+    // Number of moves played.
+    private turnCount: number = 0;
+    // State of whose turn is next.
+    private redsTurn: boolean = true;
+
+    /**
+     * Creates a new Connect4 game in the starting position (empty board) starting
+     * with red's turn.
+     */
+    public constructor() {
+        this.board = new Uint8Array(2 * ArrayConnect4Game.BOARD_SIZE);
+        this.columnHeights = new Array<number>(ArrayConnect4Game.BOARD_WIDTH).fill(0);
+    }
+
+    /**
+     * Retrieves the Connect4 board stored as a flat array.
+     *
+     * @returns An array representing the current Connect4 board state.
+     *
+     * @remarks
+     * The array has a size of `2 * BOARD_SIZE` where the first half of slots
+     * represent yellow's occupancy on the board and the second half of slots
+     * represent red's occupancy on the board.
+     */
+    public getBoard(): Uint8Array {
+        return this.board;
+    }
+
+    /**
+     * Checks whether the previous move at the given index resulted in a game win.
+     *
+     * @param index Index in the board array of the previous move.
+     * @returns True if the previous move resulted in winning the game.
+     */
+    private isWinningMove(index: number): boolean {
+        const board = this.board;
+        const localIndex = index % ArrayConnect4Game.BOARD_SIZE;
+        const row = Math.floor(localIndex / ArrayConnect4Game.BOARD_WIDTH);
+        const col = localIndex % ArrayConnect4Game.BOARD_WIDTH;
+
+        for (const [dx, dy] of ArrayConnect4Game.DIRECTIONS) {
+            let count = 1;
+            for (const dir of [-1, 1]) {
+                let x = col + dx * dir;
+                let y = row + dy * dir;
+
+                while (x >= 0 && x < ArrayConnect4Game.BOARD_WIDTH && y >= 0 && y < ArrayConnect4Game.BOARD_HEIGHT) {
+                    const i = x + y * ArrayConnect4Game.BOARD_WIDTH + (index >= ArrayConnect4Game.BOARD_SIZE
+                        ? ArrayConnect4Game.BOARD_SIZE : 0);
+                    if (!board[i]) break;
+
+                    count++;
+                    if (count >= 4)
+                        return true;
+
+                    x += dx * dir;
+                    y += dy * dir;
+                }
+            }
+        } return false;
+    }
+
+    public getTurnCount(): number {
+        return this.turnCount;
+    }
+
+    public isRedsTurn(): boolean {
+        return this.redsTurn;
+    }
+
+    public isLegalMove(column: number): boolean {
+        return this.columnHeights[column] < ArrayConnect4Game.BOARD_HEIGHT;
+    }
+
+    public playMove(column: number): boolean {
+        if (!this.isLegalMove(column))
+            throw new Error(`Move exceeds board height for column ${column}:\n${this.toString()}`);
+
+        const index = column + this.columnHeights[column] * ArrayConnect4Game.BOARD_WIDTH
+            + (this.redsTurn ? ArrayConnect4Game.BOARD_SIZE : 0);
+        this.board[index] = 1;
+
+        this.columnHeights[column]++;
+        this.redsTurn = !this.redsTurn;
+        this.turnCount++;
+
+        return this.isWinningMove(index);
+    }
+
+    public undoMove(column: number): void {
+        if (this.columnHeights[column] <= 0)
+            throw new Error(`Cannot remove piece from column ${column}:\n${this.toString()}`);
+
+        this.redsTurn = !this.redsTurn;
+        this.columnHeights[column]--;
+        this.turnCount--;
+
+        const index = column + this.columnHeights[column] * ArrayConnect4Game.BOARD_WIDTH
+            + (this.redsTurn ? ArrayConnect4Game.BOARD_SIZE : 0);
+        this.board[index] = 0;
+    }
+
+
+    public fillNeuralInput(target: Float32Array): void {
+        const board = this.board;
+        const currentOffset = this.redsTurn ? ArrayConnect4Game.BOARD_SIZE : 0;
+        const opponentOffset = this.redsTurn ? 0 : ArrayConnect4Game.BOARD_SIZE;
+        for (let i = 0; i < ArrayConnect4Game.BOARD_SIZE; i++) {
+            target[i] = board[currentOffset + i];
+            target[ArrayConnect4Game.BOARD_SIZE + i] = board[opponentOffset + i];
+        }
+    }
+
+    public toString(): string {
+        let sb = "";
+        for (let row = ArrayConnect4Game.BOARD_HEIGHT - 1; row >= 0; row--) {
+            sb += "|  ";
+            for (let column = 0; column < ArrayConnect4Game.BOARD_WIDTH; column++) {
+                const iYellow = row * ArrayConnect4Game.BOARD_WIDTH + column;
+                const iRed = iYellow + ArrayConnect4Game.BOARD_SIZE;
+                if (this.board[iYellow]) sb += "@  ";
+                else sb += this.board[iRed] ? "#  " : "-  ";
+            } sb += "|\n";
+        } return sb;
+    }
+}
--- src/connect-4/connect-4-game.ts
+++ src/connect-4/connect-4-game.ts
@@ -1,66 +1,19 @@
-/**
- * ## Connect4 Game
- * Represents an instance of the Connect4 game.
- */
-export class Connect4Game {
 
-    // Width of every Connect4 board.
-    public static readonly BOARD_WIDTH: number = 7;
-    // Height of every Connect4 board.
-    public static readonly BOARD_HEIGHT: number = 6;
-    // Number of slots of every Connect4 board.
-    public static readonly BOARD_SIZE: number = Connect4Game.BOARD_WIDTH * Connect4Game.BOARD_HEIGHT;
-
-    // Array representing the current Connect4 board state.
-    private readonly board: Uint8Array;
-    // Array indicating the current height of all columns.
-    private readonly columnHeights: number[];
-
-    // Number of moves played.
-    private turnCount: number = 0;
-    // State of whose turn is next.
-    private redsTurn: boolean = true;
-
-    /**
-     * Creates a new Connect4 game in the starting position (empty board) starting
-     * with red's turn.
-     */
-    public constructor() {
-        this.board = new Uint8Array(2 * Connect4Game.BOARD_SIZE);
-        this.columnHeights = new Array<number>(Connect4Game.BOARD_WIDTH).fill(0);
-    }
-
-    /**
-     * Retrieves the Connect4 board stored as a flat array.
-     *
-     * @returns An array representing the current Connect4 board state.
-     *
-     * @remarks
-     * The array has a size of `2 *
-     * BOARD_SIZE` where the first half of slots represent yellow's occupancy on the board and the
-     * second half of slots represent red's occupancy on the board.
-     */
-    public getBoard(): Uint8Array {
-        return this.board;
-    }
+export interface Connect4Game {
 
     /**
      * Retrieves the current turn count of the Connect4 game.
      *
      * @returns The number of moves played.
      */
-    public getTurnCount(): number {
-        return this.turnCount;
-    }
+    getTurnCount(): number;
 
     /**
      * Checks if red plays the next turn in the current Connect4 game.
      *
      * @returns True if it is red's turn.
      */
-    public isRedsTurn(): boolean {
-        return this.redsTurn;
-    }
+    isRedsTurn(): boolean;
 
     /**
      * Checks the legality of playing a piece at a given column.
@@ -68,9 +21,7 @@ export class Connect4Game {
      * @param column Index of the column to check the legality of a move.
      * @returns True if the move will not overflow the column.
      */
-    public isLegalMove(column: number): boolean {
-        return this.columnHeights[column] < Connect4Game.BOARD_HEIGHT;
-    }
+    isLegalMove(column: number): boolean;
 
     /**
      * Plays a piece in the given column as the player whose turn it is.
@@ -80,79 +31,27 @@ export class Connect4Game {
      *
      * @throws Error Throws if the move overflows the column.
      */
-    public playMove(column: number): boolean {
-        if (!this.isLegalMove(column))
-            throw new Error(`Move exceeds board height for column ${column}:\n${this.toString()}`);
-
-        const index = column + this.columnHeights[column] * Connect4Game.BOARD_WIDTH
-            + (this.redsTurn ? Connect4Game.BOARD_SIZE : 0);
-        this.board[index] = 1;
-
-        this.columnHeights[column]++;
-        this.redsTurn = !this.redsTurn;
-        this.turnCount++;
-
-        return this.isWinningMove(index);
-    }
-
-    /**
-     * Checks whether the previous move at the given index resulted in a game win.
-     *
-     * @param index Index in the board array of the previous move.
-     * @returns True if the previous move resulted in winning the game.
-     */
-    private isWinningMove(index: number): boolean {
-        // TODO: Implement this method efficiently
-        return false;
-    }
+    playMove(column: number): boolean;
 
     /**
      * Reverts the last move played in the given column.
      *
      * @param column Index of the column to revert the move.
      */
-    public undoMove(column: number): void {
-        if (this.columnHeights[column] <= 0)
-            throw new Error(`Cannot remove piece from column ${column}:\n${this.toString()}`);
-
-        this.redsTurn = !this.redsTurn;
-        this.columnHeights[column]--;
-        this.turnCount--;
-
-        const index = column + this.columnHeights[column] * Connect4Game.BOARD_WIDTH
-            + (this.redsTurn ? Connect4Game.BOARD_SIZE : 0);
-        this.board[index] = 0;
-    }
+    undoMove(column: number): void;
 
     /**
      * Fills a nn input buffer with the current board state.
      *
      * @param target Buffer to fill with the current board state.
      */
-    public fillNeuralInput(target: Float32Array): void {
-        const currentOffset = this.redsTurn ? Connect4Game.BOARD_SIZE : 0;
-        const opponentOffset = this.redsTurn ? 0 : Connect4Game.BOARD_SIZE;
-        for (let i = 0; i < Connect4Game.BOARD_SIZE; i++) {
-            target[i] = this.board[currentOffset + i];
-            target[Connect4Game.BOARD_SIZE + i] = this.board[opponentOffset + i];
-        }
-    }
+    fillNeuralInput(target: Float32Array): void;
 
     /**
      * Converts the board to its string representation.
      *
      * @returns String representation of the current board state.
      */
-    public toString(): string {
-        let sb = "";
-        for (let row = Connect4Game.BOARD_HEIGHT - 1; row >= 0; row--) {
-            sb += "|  ";
-            for (let column = 0; column < Connect4Game.BOARD_WIDTH; column++) {
-                const iYellow = row * Connect4Game.BOARD_WIDTH + column;
-                const iRed = iYellow + Connect4Game.BOARD_SIZE;
-                if (this.board[iYellow]) sb += "@  ";
-                else sb += this.board[iRed] ? "#  " : "-  ";
-            } sb += "|\n";
-        } return sb;
-    }
+    toString(): string;
 }
+
